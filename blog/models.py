3130cbb7d17111de732e6846d129f08ccc730807
Can Okan <canokan21997@gmail.com>
2019-08-15 23:12:27 +0300

    Added bootstrap and jobs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime    
 
 # Create your models here.
+
+
+class blog(models.Model):
+    title =models.CharField(max_length=20)
+    pub_data=models.DateTimeField(default=datetime.now)
+    body=models.TextField()
+    image=models.ImageField(upload_to='images/')
+    
